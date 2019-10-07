e2e4aaa6a96b0b1dceb9760c8955556a0b773e76
vascoalramos <vascoalramos@outlook.pt>
2019-10-07 16:51:35 +0100

    Exercise 9

--- lab_02/cidades.py
+++ lab_02/cidades.py
@@ -1,6 +1,6 @@
 #
 # Modulo: cidades
-# 
+#
 # Implementacao de um dominio para planeamento de caminhos
 # entre cidades usando para esse efeito o modulo tree_search
 #
@@ -10,105 +10,118 @@
 
 from tree_search import *
 
+
 class Cidades(SearchDomain):
-    def __init__(self,connections, coordinates):
+    def __init__(self, connections, coordinates):
         self.connections = connections
         self.coordinates = coordinates
-    def actions(self,cidade):
+
+    def actions(self, cidade):
         actlist = []
-        for (C1,C2,D) in self.connections:
-            if (C1==cidade):
-                actlist += [(C1,C2)]
-            elif (C2==cidade):
-               actlist += [(C2,C1)]
-        return actlist 
-    def result(self,cidade,action):
-        (C1,C2) = action
-        if C1==cidade:
+        for (C1, C2, D) in self.connections:
+            if (C1 == cidade):
+                actlist += [(C1, C2)]
+            elif (C2 == cidade):
+                actlist += [(C2, C1)]
+        return actlist
+
+    def result(self, cidade, action):
+        (C1, C2) = action
+        if C1 == cidade:
             return C2
-    def cost(self, state, action):
-        pass
+
+    def cost(self, cidade, action): # implementation of cost function (7)
+        (C1, C2) = action
+        if C1 != cidade:
+            return None
+        for c1, c2, dist in self.connections:
+            if (c1, c2) == action or (c2, c1) == action:
+                return dist
+        return None
+
     def heuristic(self, state, goal_state):
         pass
 
-cidades_portugal = Cidades( 
-                    # Ligacoes por estrada
-                    [
-                      ('Coimbra', 'Leiria', 73),
-                      ('Aveiro', 'Agueda', 35),
-                      ('Porto', 'Agueda', 79),
-                      ('Agueda', 'Coimbra', 45),
-                      ('Viseu', 'Agueda', 78),
-                      ('Aveiro', 'Porto', 78),
-                      ('Aveiro', 'Coimbra', 65),
-                      ('Figueira', 'Aveiro', 77),
-                      ('Braga', 'Porto', 57),
-                      ('Viseu', 'Guarda', 75),
-                      ('Viseu', 'Coimbra', 91),
-                      ('Figueira', 'Coimbra', 52),
-                      ('Leiria', 'Castelo Branco', 169),
-                      ('Figueira', 'Leiria', 62),
-                      ('Leiria', 'Santarem', 78),
-                      ('Santarem', 'Lisboa', 82),
-                      ('Santarem', 'Castelo Branco', 160),
-                      ('Castelo Branco', 'Viseu', 174),
-                      ('Santarem', 'Evora', 122),
-                      ('Lisboa', 'Evora', 132),
-                      ('Evora', 'Beja', 105),
-                      ('Lisboa', 'Beja', 178),
-                      ('Faro', 'Beja', 147),
-                      # extra
-                      ('Braga', 'Guimaraes', 25),
-                      ('Porto', 'Guimaraes', 44),
-                      ('Guarda', 'Covilha', 46),
-                      ('Viseu', 'Covilha', 57),
-                      ('Castelo Branco', 'Covilha', 62),
-                      ('Guarda', 'Castelo Branco', 96),
-                      ('Lamego','Guimaraes', 88),
-                      ('Lamego','Viseu', 47),
-                      ('Lamego','Guarda', 64),
-                      ('Portalegre','Castelo Branco', 64),
-                      ('Portalegre','Santarem', 157),
-                      ('Portalegre','Evora', 194) ],
 
-                    # Coordenadas das cidades:
-                     { 'Aveiro': (41,215),
-                       'Figueira': ( 24, 161),
-                       'Coimbra': ( 60, 167),
-                       'Agueda': ( 58, 208),
-                       'Viseu': ( 104, 217),
-                       'Braga': ( 61, 317),
-                       'Porto': ( 45, 272),
-                       'Lisboa': ( 0, 0),
-                       'Santarem': ( 38, 59),
-                       'Leiria': ( 28, 115),
-                       'Castelo Branco': ( 140, 124),
-                       'Guarda': ( 159, 204),
-                       'Evora': (120, -10),
-                       'Beja': (125, -110),
-                       'Faro': (120, -250),
-                       #extra
-                       'Guimaraes': ( 71, 300),
-                       'Covilha': ( 130, 175),
-                       'Lamego' : (125,250),
-                       'Portalegre': (130,170) }
-                     )
+cidades_portugal = Cidades(
+    # Ligacoes por estrada
+    [
+        ('Coimbra', 'Leiria', 73),
+        ('Aveiro', 'Agueda', 35),
+        ('Porto', 'Agueda', 79),
+        ('Agueda', 'Coimbra', 45),
+        ('Viseu', 'Agueda', 78),
+        ('Aveiro', 'Porto', 78),
+        ('Aveiro', 'Coimbra', 65),
+        ('Figueira', 'Aveiro', 77),
+        ('Braga', 'Porto', 57),
+        ('Viseu', 'Guarda', 75),
+        ('Viseu', 'Coimbra', 91),
+        ('Figueira', 'Coimbra', 52),
+        ('Leiria', 'Castelo Branco', 169),
+        ('Figueira', 'Leiria', 62),
+        ('Leiria', 'Santarem', 78),
+        ('Santarem', 'Lisboa', 82),
+        ('Santarem', 'Castelo Branco', 160),
+        ('Castelo Branco', 'Viseu', 174),
+        ('Santarem', 'Evora', 122),
+        ('Lisboa', 'Evora', 132),
+        ('Evora', 'Beja', 105),
+        ('Lisboa', 'Beja', 178),
+        ('Faro', 'Beja', 147),
+        # extra
+        ('Braga', 'Guimaraes', 25),
+        ('Porto', 'Guimaraes', 44),
+        ('Guarda', 'Covilha', 46),
+        ('Viseu', 'Covilha', 57),
+        ('Castelo Branco', 'Covilha', 62),
+        ('Guarda', 'Castelo Branco', 96),
+        ('Lamego', 'Guimaraes', 88),
+        ('Lamego', 'Viseu', 47),
+        ('Lamego', 'Guarda', 64),
+        ('Portalegre', 'Castelo Branco', 64),
+        ('Portalegre', 'Santarem', 157),
+        ('Portalegre', 'Evora', 194)],
+
+    # Coordenadas das cidades:
+    {'Aveiro': (41, 215),
+     'Figueira': (24, 161),
+     'Coimbra': (60, 167),
+     'Agueda': (58, 208),
+     'Viseu': (104, 217),
+     'Braga': (61, 317),
+     'Porto': (45, 272),
+     'Lisboa': (0, 0),
+     'Santarem': (38, 59),
+     'Leiria': (28, 115),
+     'Castelo Branco': (140, 124),
+     'Guarda': (159, 204),
+     'Evora': (120, -10),
+     'Beja': (125, -110),
+     'Faro': (120, -250),
+     # extra
+     'Guimaraes': (71, 300),
+     'Covilha': (130, 175),
+     'Lamego': (125, 250),
+     'Portalegre': (130, 170)}
+)
 
 
+p = SearchProblem(cidades_portugal, 'Braga', 'Faro')
+t = SearchTree(p, 'uniform')
 
-
-p = SearchProblem(cidades_portugal,'Braga','Faro')
-t = SearchTree(p,'depth')
 
 # Atalho para obter caminho de c1 para c2 usando strategy:
-def search_path(c1,c2,strategy):
-    my_prob = SearchProblem(cidades_portugal,c1,c2)
+def search_path(c1, c2, strategy):
+    my_prob = SearchProblem(cidades_portugal, c1, c2)
     my_tree = SearchTree(my_prob)
     my_tree.strategy = strategy
     return my_tree.search()
 
-print(t.search(9))
-print(F"Length: {t.length}") # added to alinea (c)
-print(f"Terminal nodes: {t.terminal}") # added to alinea (5)
-print(f"Non terminal nodes: {t.non_terminal}") # added to alinea (5)
-print(f"Ramification ratio: {t.ramification}") # added to alinea (6)
+
+print(t.search(8))
+print(F"Length: {t.length}")  # added to alinea (3)
+print(f"Terminal nodes: {t.terminal}")  # added to alinea (5)
+print(f"Non terminal nodes: {t.non_terminal}")  # added to alinea (5)
+print(f"Ramification ratio: {t.ramification}")  # added to alinea (6)
+print(f"Total cost: {t.cost}")  # added to alinea (9)
--- lab_02/tree_search.py
+++ lab_02/tree_search.py
@@ -1,12 +1,12 @@
 
 # Modulo: tree_search
-# 
-# Fornece um conjunto de classes para suporte a resolucao de 
+#
+# Fornece um conjunto de classes para suporte a resolucao de
 # problemas por pesquisa em arvore:
 #    SearchDomain  - dominios de problemas
-#    SearchProblem - problemas concretos a resolver 
+#    SearchProblem - problemas concretos a resolver
 #    SearchNode    - nos da arvore de pesquisa
-#    SearchTree    - arvore de pesquisa, com metodos para 
+#    SearchTree    - arvore de pesquisa, com metodos para
 #                    a respectiva construcao
 #
 #  (c) Luis Seabra Lopes
@@ -18,6 +18,8 @@ from abc import ABC, abstractmethod
 # Dominios de pesquisa
 # Permitem calcular
 # as accoes possiveis em cada estado, etc
+
+
 class SearchDomain(ABC):
 
     # construtor
@@ -53,22 +55,27 @@ class SearchProblem:
         self.domain = domain
         self.initial = initial
         self.goal = goal
+
     def goal_test(self, state):
         return state == self.goal
 
 
 # Nos de uma arvore de pesquisa
 class SearchNode:
-    def __init__(self,state,parent, depth): 
+    def __init__(self, state, parent, depth, cost):
         self.state = state
         self.parent = parent
         self.depth = depth
-    def in_parent(self, state): # check the parent node so that we prevent the infinite loop
+        self.cost = cost
+
+    def in_parent(self, state):  # check the parent node so that we prevent the infinite loop
         if self.parent is None:
             return False
         return self.state == state or self.parent.in_parent(state)
+
     def __str__(self):
-        return f"no({self.state}, {self.depth})"
+        return f"no({self.state}, {self.depth}, {self.cost})"
+
     def __repr__(self):
         return str(self)
 
@@ -77,21 +84,22 @@ class SearchNode:
 class SearchTree:
 
     # construtor
-    def __init__(self,problem, strategy='breadth'): 
+    def __init__(self, problem, strategy='breadth'):
         self.problem = problem
-        root = SearchNode(problem.initial, None, 0)
+        root = SearchNode(problem.initial, None, 0, 0)
         self.open_nodes = [root]
         self.strategy = strategy
         self.length = 0
         self.terminal = 0
         self.non_terminal = 1
+        self.cost = 0
 
     # obter o caminho (sequencia de estados) da raiz ate um no
-    def get_path(self,node):
+    def get_path(self, node):
         if node.parent == None:
-            return [node.state]
+            return [node]
         path = self.get_path(node.parent)
-        path += [node.state]
+        path += [node]
         return(path)
 
     # procurar a solucao
@@ -99,16 +107,19 @@ class SearchTree:
         while self.open_nodes != []:
             node = self.open_nodes.pop(0)
             if self.problem.goal_test(node.state):
-                self.ramification = (self.terminal + self.non_terminal - 1 ) / self.non_terminal
+                self.ramification = (
+                    self.terminal + self.non_terminal - 1) / self.non_terminal
                 return self.get_path(node)
             lnewnodes = []
             for a in self.problem.domain.actions(node.state):
-                newstate = self.problem.domain.result(node.state,a)
+                newstate = self.problem.domain.result(node.state, a)
                 # Add the node if this does not already exist (we are now skippng the loop)
                 # The second condition is to skip iteration if the limit threshold was passed
                 if not node.in_parent(newstate) and node.depth < limit:
-                    lnewnodes += [SearchNode(newstate,node,node.depth+1)]
+                    lnewnodes += [SearchNode(newstate, node, node.depth+1,
+                                             node.cost + self.problem.domain.cost(node.state, a))]
                     self.length += 1
+                    self.cost += self.problem.domain.cost(node.state, a)
             self.add_to_open(lnewnodes)
             self.non_terminal += len(lnewnodes)
             if lnewnodes == []:
@@ -117,11 +128,10 @@ class SearchTree:
         return None
 
     # juntar novos nos a lista de nos abertos de acordo com a estrategia
-    def add_to_open(self,lnewnodes):
+    def add_to_open(self, lnewnodes):
         if self.strategy == 'breadth':
             self.open_nodes.extend(lnewnodes)
         elif self.strategy == 'depth':
             self.open_nodes[:0] = lnewnodes
         elif self.strategy == 'uniform':
-            pass
-
+            self.open_nodes = sorted(self.open_nodes + lnewnodes, key = lambda x: x.cost)
